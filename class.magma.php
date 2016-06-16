ed0cb2366abeca6221f1ec4d0726a30d72392702
Inxk <contact@inxk.eu>
2016-06-16 19:23:10 +0200

    Added constructor for init config vars

--- class.magma.php
+++ class.magma.php
@@ -1,4 +1,18 @@
 <?php
+/**************************************************************************
+This file is part of Magma.
+Magma is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+Magma is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with Magma. If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
 namespace Magma;
 
 define("DS", DIRECTORY_SEPARATOR);
@@ -177,6 +191,23 @@ class Magma{
 
 		return $data;
 
+	}
+
+	/**
+	 * Constructor init config vars
+	 * @param array $vars
+	 * @return true
+	 */
+
+	public function __construct($vars=[]){
+
+		if(!empty($vars) && is_array($vars)){
+			foreach($vars as $k=>$v){
+				$this->$k = $v;
+			}
+			return true;
+		}
+		return false;
 	}
 
 	/**
