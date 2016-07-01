8149d96710dfd3bfb0d80b8ee4a81191ac0e7431
Inxk <contact@inxk.eu>
2016-07-01 10:54:49 +0200

    Revert "Added support for PHP >= 5.6"
    This reverts commit e0df26509740adc0ce47d92089332ac7ded9b4d1.

--- class.magma.php
+++ class.magma.php
@@ -1,6 +1,6 @@
 <?php
 /**************************************************************************
-						This file is part of Magma.
+This file is part of Magma.
 Magma is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
