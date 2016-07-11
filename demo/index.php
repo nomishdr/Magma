47768917c2e367abb9e3c70751c218d81c8fa8c8
Houdayer Simon <houdayersimon@gmail.com>
2016-07-11 22:03:56 +0200

    Added Demo folder

---
+++ demo/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<title>Magma Demo</title>
+
+	</head>
+<body>
+<?php
+require '../class.magma.php';
+
+$Magma = new Inxk\Magma();
+$Magma->debug = true;
+
+//$Magma->new("MyDB");
+
+$Magma->use("MyDB");
+
+//$Magma->create("MyTable", ["id"=>"int,auto_increment","firstname"=>"text","lastname"=>"text","date"=>"date"]);
+
+$Magma->load("MyTable");
+$Magma->insert(
+    ['', "Firstname1", "Lastname1", "d-m-Y"], // "d-m-y" will be replace by the date (e.g 15-06-2016)
+    ['', "Firstname2", "Lastname2", ""], //4 column will be fill by the current date in English format (e.g 2016-06-15)
+    ['', "Firstname3", "Lastname3", "2017-08-01"] //This also works :)
+    //...
+);
+$result = $Magma->fetch(); //Fetch with no conditions
+	print_r($result);
+$result = $Magma->fetch(["firstname"=>"Firstname1"]); //Find every firstname equal to "Firstname1"
+	print_r($result);
+$result = $Magma->fetch(["firstname"=>"Firstname1"], ["LIMIT"=>1]); //Find the first first name equal to "Firstname1"
+	print_r($result);
+
+$Magma->update(["firstname"=>"New firstname ! :D"], ["id"=>3]); //Find id=3 and replace its firstname with the associate value
+
+$Magma->delete(["id"=>1]); //Delete record where id=1
+?>
+</body>
+</html>
